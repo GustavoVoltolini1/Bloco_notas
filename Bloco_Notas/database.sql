e59c9daee2c2f0b8d112f86bde23ae8c0e576ab0
Otavio101 <otavio_roecker@estudante.sesisenai.org.br>
2024-09-30 09:25:29 -0300

    sql e img modelo conceitual

---
+++ Bloco_Notas/database.sql
@@ -0,0 +1,17 @@
+Create database BlocoNotas;
+Use BlocoNotas;
+
+create table user (
+user_id INT auto_increment primary key,
+    nome_usuario VARCHAR(50) NOT NULL
+    );
+
+
+create table notas(
+nota_id INT auto_increment primary Key,
+    user_id INT NOT NULL,
+    titulo VARCHAR(50) NOT NULL,
+    categoria VARCHAR(50) NOT NULL,
+    conteudo TEXT not null,
+    FOREIGN KEY (user_id) REFERENCES USER(user_id)
+    );
