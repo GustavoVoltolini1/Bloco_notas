f394452d1a19016165c4400784fc3dafd31c434e
GustavoVoltolini1 <gustavo_d_voltolini@estudante.sesisenai.org.br>
2024-09-30 09:24:38 -0300

    fiz o db create read

---
+++ Bloco_Notas/Read.php
@@ -0,0 +1,8 @@
+<?php
+$result = $conn->query("SELECT * FROM notas");
+while ($row = $result->fetch_assoc()) {
+    echo "ID: " . $row["nota_id"] . " - Título: " . $row["titulo"] . "<br>";
+    echo "Categoria: " . $row["categoria"] . "<br>";
+    echo "Conteúdo: " . $row["conteudo"] . "<br><br>";
+}
+?>
